f6157507020013ae67fedae31a70b1ffd104dab3
Matkins15 <matthew1atkins@gmail.com>
2017-04-27 10:10:13 -0400

    showing posts on city page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
-  get '/' => 'cities#index'
-  get '/cities/:id' => 'cities#show'
+    get '/' => 'cities#index'
+
+    # cities
+    resources :cities do
+        resources :posts
+    end
+    get '/cities/:id/posts' => 'cities#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
