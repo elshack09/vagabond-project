271eb6ebb89e8329b63c45b51d414df71407a506
aylinmarie <aylinmariedesigns@gmail.com>
2017-04-27 16:43:51 -0400

    confirm message for delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,33 +5,41 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+     @confirm = "Are you sure you want to delete this review: #{@post.title} ?"
   end
 
   def new
-    @post = Post.new
     @city = City.find(params[:city_id])
+    @post = Post.new
+    render :new
+
   end
 
   def create
-    @post = Post.new(post_params)
-    if post.save
-      city = City.find(params[:city_id])
-      city.posts << @post
-      render json: @post, status: :created
+    @city = City.find(params[:city_id])
+
+    @post = Post.create(post_params)
+    if @post.save
+      redirect_to city_post_path(@city, @post.id)
     else
-      render json: @post, status: :uporocessable_entity
+      redirect_to city_path(@city)
     end
+
   end
 
   def destroy
+    @city = City.find(params[:city_id])
+
     @post = Post.find(params[:id])
     @post.destroy
-    redirect_to cities_path
+    flash[:notice] = "'#{@post.title}' post was deleted"
+    redirect_to city_path(@city)
   end
 
   private
     def post_params
       params.require(:post).permit(:title, :content)
+
     end
 
 end
