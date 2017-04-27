2fa721f838d86639a7117fda8a0b58a88d8a37d9
aylinmarie <aylinmariedesigns@gmail.com>
2017-04-27 14:34:05 -0400

    added delete button

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,12 +13,23 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(post_params)
+    @post = Post.new(post_params)
+    if post.save
+      city = City.find(params[:city_id])
+      city.posts << @post
+      render json: @post, status: :created
+    else
+      render json: @post, status: :uporocessable_entity
+    end
+  end
+
+  def destroy
+
   end
 
   private
     def post_params
-      params.require(:post).permit(:title)
+      params.require(:post).permit(:title, :content)
     end
 
 end
