79034b1b6db975660cb44d6f569576a346e1f3ed
aylinmarie <aylinmariedesigns@gmail.com>
2017-04-26 21:15:11 -0400

    added posts to cities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170427004400) do
+ActiveRecord::Schema.define(version: 20170427010839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20170427004400) do
     t.datetime "updated_at", null: false
     t.string   "name"
     t.string   "photo_url"
+    t.integer  "post_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
