1f0ebd3697fb3332a367f4bf5e3d44b182b103a0
Matkins15 <matthew1atkins@gmail.com>
2017-04-27 18:14:04 -0400

    truly resolved routes.rb conflict

--- config/routes.rb
+++ config/routes.rb
@@ -6,21 +6,5 @@ Rails.application.routes.draw do
     resources :cities do
         resources :posts
     end
-<<<<<<< HEAD
 
-
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-=======
-    # get '/cities/:id' => 'cities#show'
-    #
-    # #posts
-    # get '/cities/:city_id/posts' => 'posts#index'
-    # post '/cities/:city_id/posts' => 'posts#create'
-    # get '/cities/:city_id/posts/new' => 'posts#new'
-    # get '/cities/:city_id/posts/:id' => 'posts#show'
-    # delete '/cities/:city_id/posts/:id' => 'posts#destroy'
-
->>>>>>> 729597c1b60fa9cb64212fc4bf64c43000efd377
 end
