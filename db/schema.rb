b86ecf4df0e305c4f3584a24ccd6e79aae6bdf21
aylinmarie <aylinmariedesigns@gmail.com>
2017-04-26 20:44:39 -0400

    created model for city

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170427004400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "name"
+    t.string   "photo_url"
+  end
 
 end
