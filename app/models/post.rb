44f513f2bc401f74f8a11ad47805f34ee6968dda
Matkins15 <matthew1atkins@gmail.com>
2017-04-28 12:33:19 -0400

    user controller setup and profile page accessible

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   include ActiveModel::ForbiddenAttributesProtection
 
   belongs_to :city
+  belongs_to :user
   validates :content, presence: true
   validates :title, presence: true
 
