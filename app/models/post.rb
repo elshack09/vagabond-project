5c01d934f1f2aee3304873dfe7a5683d650a9042
Matkins15 <matthew1atkins@gmail.com>
2017-04-28 14:37:36 -0400

    user is able to save post in profile and on city page

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,9 @@ class Post < ApplicationRecord
 
   belongs_to :city
   belongs_to :user
-  validates :content, presence: true
-  validates :title, presence: true
+  validates :title, :content, presence: true
 
-  validates_length_of :title, :within => 1..200, :too_long => "pick a shorter title", :too_short => "pick a longer title"
+
+  # validates_length_of :title, :within => 1..200, :too_long => "pick a shorter title", :too_short => "pick a longer title"
 
 end
