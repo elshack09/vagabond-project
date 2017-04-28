d8412521f74fa023211574adc37c1dd812e4b004
Matkins15 <matthew1atkins@gmail.com>
2017-04-28 11:03:57 -0400

    added validates_length_of to Post method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ApplicationRecord
   include ActiveModel::ForbiddenAttributesProtection
 
   belongs_to :city
-  validates :title, :content, presence: true
+  validates :content, presence: true
+  validates :title, presence: true
+  validates_length_of :title, :within => 1..200, :too_long => "pick a shorter title", :too_short => "pick a longer title"
 
 end
