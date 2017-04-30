996a3e454c6fb815aa8080de341b74d048aa399a
aylinmarie <aylinmariedesigns@gmail.com>
2017-04-29 19:53:58 -0500

    add user edit page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170428165242) do
+ActiveRecord::Schema.define(version: 20170430004645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20170428165242) do
     t.string   "phone_number"
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "photo_url"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
